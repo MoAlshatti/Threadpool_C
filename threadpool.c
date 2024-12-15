f1c2cc07c2be4b0b47317d71207a0bc3c097e1eb
MoAlshatti <lg22416@bristol.ac.uk>
2024-12-15 11:11:26 +0000

    created a queue

--- threadpool.c
+++ threadpool.c
@@ -3,8 +3,43 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdbool.h>
+#define THREADS_NUMBER 4
 
+pthread_t threads [THREADS_NUMBER];
 
+typedef struct {
+    void *func_ptr;
+    // args (add at some point)
+    task *next;
+}task;
+
+typedef struct {
+    task *front;
+    task *rear;
+
+}tasksQue;
+
+tasksQue taskQueue = (tasksQue) {.front = NULL, .rear = NULL};
+
+void EnqueueTask(task *task){
+    taskQueue.rear->next = task;
+    taskQueue.rear = task;
+}
+
+task *DequeueTask(){
+    task *t = taskQueue.front;
+    taskQueue.front = taskQueue.front->next;
+    return t;
+}
+
+bool isEmpty(){
+    if (taskQueue.front == NULL && taskQueue.rear == NULL){
+        return true;
+    }  else {
+        return false;
+    }
+}
 
 void main (void){
     
